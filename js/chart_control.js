9a576181bacccef57019250a255b6d2c6f165eed
Jessika Nunes Caetano <jessikanunescaetano@gmail.com>
2018-03-26 00:04:21 -0300

    McCabe-Thiele
    Método de McCabe implantado, faltando apenas finalizações da parte 1 (conversão massa-molar, apresentação dos resultados, restrições de entrada e exemplos ideal e não ideal)

--- js/chart_control.js
+++ js/chart_control.js
@@ -1,34 +1,88 @@
  // Arquivo de controle do gráfico
- function gerar_grafico() {
+ function gerar_grafico(duration) {
 
    // Limpeza da div que contém o gráfico, criação do canvas e adição do gráfico no mesmo
    $("#div_chart").empty();
-   var ctx2 = document.createElement("canvas")
+   var ctx2 = document.createElement("canvas");
    ctx2.setAttribute("id", "myChart");
    ctx2.setAttribute("height", "250px");
    div_chart.appendChild(ctx2);
+
    //declaração da variável do chart
    var ctx = document.getElementById("myChart");
 
    //Criação das variáveis contendo os dados do gráfico
-   var linha_aux = []
-   var curva_eq = []
+   var linha_aux = [];
+   var curva_eq = [];
+   var retificacao = [];
+   var esgotamento = [];
+   var alimentacao = [];
+   var degraus = [];
 
    // Adição das séries de dados no gráfico
    for (i = 0; i <= 100; i++) {
 
-     var aux = {
+     var aux_0 = {
        x: xvolatil[i],
        y: xvolatil[i]
      };
 
-     var aux1 = {
+     var aux_1 = {
        x: xvolatil[i],
        y: yvolatil[i]
      };
 
-     linha_aux.push(aux);
-     curva_eq.push(aux1);
+     linha_aux.push(aux_0);
+     curva_eq.push(aux_1);
+
+   }
+
+   var aux_2 = {
+     x: xD,
+     y: xD
+   };
+
+   var aux_3 = {
+     x: xB,
+     y: xB
+   };
+
+   var aux_4 = {
+     x: xF,
+     y: xF
+   };
+
+   retificacao.push(aux_2);
+   esgotamento.push(aux_3);
+   alimentacao.push(aux_4);
+
+   var aux_2 = {
+     x: xF,
+     y: yK
+   };
+
+   var aux_3 = {
+     x: xF,
+     y: yK
+   };
+
+   var aux_4 = {
+     x: xF,
+     y: yK
+   };
+
+   retificacao.push(aux_2);
+   esgotamento.push(aux_3);
+   alimentacao.push(aux_4);
+
+   for (i = 0; i < x_degrau.length; i++) {
+
+     var aux_5 = {
+       x: x_degrau[i],
+       y: y_degrau[i]
+     };
+
+     degraus.push(aux_5);
 
    }
 
@@ -49,7 +103,7 @@
            pointRadius: 0.1
          },
          {
-           label: "Curva Auxiliar",
+           label: "z",
            backgroundColor: '#4DB6AC',
            showLine: true,
            fill: false,
@@ -57,27 +111,92 @@
            borderColor: '#4DB6AC',
            borderWidth: 2,
            pointRadius: 0.1
+         },
+         {
+           label: "z",
+           backgroundColor: '#b0bec5',
+           showLine: true,
+           fill: false,
+           data: retificacao,
+           borderColor: '#b0bec5',
+           borderWidth: 2,
+           pointRadius: 0.1
+         },
+         {
+           label: "z",
+           backgroundColor: '#b0bec5',
+           showLine: true,
+           fill: false,
+           data: esgotamento,
+           borderColor: '#b0bec5',
+           borderWidth: 2,
+           pointRadius: 0.1
+         },
+         {
+           label: "z",
+           backgroundColor: '#78909c',
+           showLine: true,
+           fill: false,
+           data: alimentacao,
+           borderColor: '#78909c',
+           borderWidth: 2,
+           pointRadius: 0.1
+         },
+         {
+           label: "Estágios",
+           backgroundColor: '#78909c',
+           showLine: true,
+           fill: false,
+           data: degraus,
+           borderColor: '#78909c',
+           borderWidth: 2,
+           pointRadius: 0.1
          }
        ]
      },
 
+     // Configurações do gráfico
      options: {
+       legend: {
+         labels: {
+           // Remover legenda de alguns datasets
+           filter: function(item, myLineChart) {
+             return !item.text.includes("z");
+           }
+         }
+       },
+       animation: {
+         duration: duration
+       },
        scales: {
          yAxes: [{
+           scaleLabel: {
+             display: true,
+             labelString: 'y (fração molar)'
+           },
            ticks: {
              max: 1,
              min: 0
            }
          }],
          xAxes: [{
+           scaleLabel: {
+             display: true,
+             labelString: 'x (fração molar)'
+           },
            ticks: {
              max: 1,
              min: 0
            }
          }]
+       },
+       elements: {
+         line: {
+           tension: 0
+         }
        }
      }
-     
+
    });
 
  }
--- js/destilacao.js
+++ js/destilacao.js
@@ -5,38 +5,38 @@ $(document).ready(function() {
 });
 
 //Desmarcação dos radio buttons quando atualizar a página
-$("#tipo_mistura1").attr("checked", false)
-$("#tipo_mistura2").attr("checked", false)
-$("#metodo1").attr("checked", false)
-$("#metodo2").attr("checked", false)
+$("#tipo_mistura1").attr("checked", false);
+$("#tipo_mistura2").attr("checked", false);
+$("#metodo1").attr("checked", false);
+$("#metodo2").attr("checked", false);
 
 //Inserção dos dados na caixa de rolagem do componentes 1 e 2
-select_componentes = document.getElementById("select_componentes")
-var opt = document.createElement("option")
+select_componentes = document.getElementById("select_componentes");
+var opt = document.createElement("option");
 opt.value = 0;
 opt.text = 'Escolha uma opção';
-opt.selected = 'selected'
-opt.disabled = 'disabled'
-select_componentes.add(opt, select_componentes.options[0])
+opt.selected = 'selected';
+opt.disabled = 'disabled';
+select_componentes.add(opt, select_componentes.options[0]);
 for (i = 0; i < data.componentes.length; i++) {
-  var opt = document.createElement("option")
-  opt.value = i
-  opt.text = data.componentes[i]
-  select_componentes.add(opt, select_componentes.options[i + 1])
+  var opt = document.createElement("option");
+  opt.value = i;
+  opt.text = data.componentes[i];
+  select_componentes.add(opt, select_componentes.options[i + 1]);
 }
 
-select_componentes2 = document.getElementById("select_componentes2")
-var opt2 = document.createElement("option")
+select_componentes2 = document.getElementById("select_componentes2");
+var opt2 = document.createElement("option");
 opt2.value = 0;
 opt2.text = 'Escolha uma opção';
-opt2.selected = 'selected'
-opt2.disabled = 'disabled'
-select_componentes2.add(opt2, select_componentes2.options[0])
+opt2.selected = 'selected';
+opt2.disabled = 'disabled';
+select_componentes2.add(opt2, select_componentes2.options[0]);
 for (i = 0; i < data.componentes.length; i++) {
-  var opt = document.createElement("option")
-  opt.value = i
-  opt.text = data.componentes[i]
-  select_componentes2.add(opt, select_componentes2.options[i + 1])
+  var opt = document.createElement("option");
+  opt.value = i;
+  opt.text = data.componentes[i];
+  select_componentes2.add(opt, select_componentes2.options[i + 1]);
 }
 
 //Inserção dos dados na caixa de rolagem dos métodos de atividade
@@ -55,19 +55,24 @@ for (i = 0; i < data.metodos_atividade.length; i++) {
 }
 
 // Início da programação dos cálculos com a declaração das variáveis
-var pressao = 101.325
-var temperaturas = Array()
-var P1sat = Array()
-var P2sat = Array()
-var xvolatil = Array()
-var yvolatil = Array()
-var alfa_ideal = Array()
-var A1, A2, B1, B2, C1, C2, T1, Psat_comp1, Psat_comp2, atividade1, atividade2, x1, x2
-var tipo_mistura, metodo_atividade, metodo_grafico, metodo_entalpia, componente1, componente2
-var comp_alimentacao, comp_topo, comp_fundo
-var aux2 = Array()
-var aux3 = Array()
-var metodos_atividade = Array()
+var pressao = 101.325;
+var temperaturas = Array(),
+  xvolatil = Array(),
+  yvolatil = Array(),
+  yvolatil_2 = Array(),
+  alfa_ideal = Array();
+var metodos_atividade = Array();
+var ek1 = Array(),
+  ek2 = Array(),
+  id1 = Array(),
+  p_interacao = Array();
+
+var x_degrau = Array(),
+  y_degrau = Array();
+var A1, A2, B1, B2, C1, C2, T1sat, T2sat, T1, P1sat, P2sat, atividade1, atividade2, x1, x2, qk1_total, qk2_total, rk1_total, rk2_total;
+var tipo_mistura, metodo_atividade, metodo_grafico, metodo_entalpia, componente1, componente2;
+var xF, xD, xB, Rd_min, Rd, yF,x_aux, y_aux;
+
 
 //link dos comandos de cálculo com o botão
 function botao_calcular() {
@@ -75,41 +80,53 @@ function botao_calcular() {
   //Alerta caso o usuário insira dois componentes idêncticos
   if (componente1 == componente2) {
 
-    alert("Por favor, escolha dois componentes distintos.")
+    alert("Por favor, escolha dois componentes distintos.");
 
   } else {
 
     //Inserção dos nomes dos componentes a partir das caixas de rolagem
-    componente1 = data.componentes[document.getElementById("select_componentes").value]
-    componente2 = data.componentes[document.getElementById("select_componentes2").value]
+    componente1 = data.componentes[document.getElementById("select_componentes").value];
+    componente2 = data.componentes[document.getElementById("select_componentes2").value];
 
     //Inserção do tipo de mistura a partir da Check-Box
     aux2 = document.getElementsByName("grupo1")
     for (var i = 0; i < aux2.length; i++) {
 
       if (aux2[i].checked == true) {
-        tipo_mistura = aux2[i].value
+        tipo_mistura = aux2[i].value;
       }
 
     }
 
     //Inserção do método de atividade a partir da caixa de rolagem
-    metodo_atividade = metodos_atividade[document.getElementById("novo_select").value]
+    metodo_atividade = metodos_atividade[document.getElementById("novo_select").value];
+    document.getElementById('range_element').value = 10;
 
+    xF = 0.44;
+    xB = 0.0235;
+    xD = 0.8;
     //Chamada da função de cálculo da curva de ELV de acordo com o tipo de mistura
     if (tipo_mistura == "Mistura Ideal") {
 
-      curvaeq_ideal()
-      gerar_grafico()
+      curvaeq_ideal();
+      McCabe_Ideal();
+      gerar_grafico(700);
 
     } else if (tipo_mistura == "Mistura Não Ideal") {
 
-      curvaeq_naoideal()
-      gerar_grafico()
+      curvaeq_naoideal();
+
+      if (xD < xmax && xF < xmax) {
+        McCabe_NIdeal();
+      } else {
+        alert("Esta mistura não pode ser destilada até concentrações maiores que " + xmax.toFixed(2) + " devido à formação de azeótropo.")
+      }
+
+      gerar_grafico(700);
 
     } else {
 
-      alert("Por favor, defina o tipo da mistura escolhida.")
+      alert("Por favor, defina o tipo da mistura escolhida.");
 
     }
   }
